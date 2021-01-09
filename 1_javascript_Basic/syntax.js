faf66e1fc3298d004078fe82c5bf75299b329ce1
IOUIOU50 <wikis2101@naver.com>
2021-01-09 18:42:57 +0900

    syntax

---
+++ 1_javascript_Basic/syntax.js
@@ -0,0 +1,17 @@
+var a = 1;
+b = 2; //변수를 나타내는 var키워드 없이도 새로운 변수가 만들어진다.
+
+console.log("a : " + a);
+console.log("b : " + b);
+
+var c = 3, d = 10;
+var str1 = "hello";
+var str2 = "Javascript";
+var str3 = null;
+
+console.log("c : " + c);
+console.log("d : " + d);
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
